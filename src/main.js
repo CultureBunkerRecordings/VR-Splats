b6ea3c7e8183c20374e9e3e4456dc1ada174fd21
signalssoundsandwaves <signalssoundsandwaves@gmail.com>
2025-11-29 17:26:34 +0000

    first VR test

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import * as GaussianSplats3D from '@mkkellogg/gaussian-splats-3d';
+
+const viewer = new GaussianSplats3D.Viewer({
+    'cameraUp': [0, -1, 0],
+    'initialCameraPosition': [1.6, 0, 0],
+    'initialCameraLookAt': [0, 0, 0]
+});
+viewer.addSplatScene('/thereminSplat.splat', {
+    'splatAlphaRemovalThreshold': 5,
+    'showLoadingUI': true,
+    'position': [0, 1, 0],
+    'rotation': [0, 0, 0, 1],
+    'scale': [1.5, 1.5, 1.5],
+     'webXRMode': GaussianSplats3D.WebXRMode.AR,
+})
+.then(() => {
+    viewer.start();
+});
