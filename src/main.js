092c97a2ebc18120151fa37b6d237b8da68b7957
signalssoundsandwaves <signalssoundsandwaves@gmail.com>
2025-11-30 22:28:01 +0000

    AR?

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ viewer.addSplatScene('/gs_TheseusAndMinotaurLuma.ply', {
     'position': [0, 1, 0],
     'rotation': [0, 0, 0, 1],
     'scale': [1.5, 1.5, 1.5],
-    'webXRMode': GaussianSplats3D.WebXRMode.VR
+    'webXRMode': GaussianSplats3D.WebXRMode.AR
 })
 .then(() => {
     viewer.start();
